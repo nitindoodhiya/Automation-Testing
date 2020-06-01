8ca0c1ab2f948411c7644eb5c21328e8000e620a
Nick Nitin <nitindoodhiya@gmail.com>
2020-06-01 11:19:56 +0530

    comments

--- src/test/java/Base/YMLReader.java
+++ src/test/java/Base/YMLReader.java
@@ -1,19 +1,21 @@
 package Base;
 
 import org.yaml.snakeyaml.Yaml;
-
 import java.io.InputStream;
 import java.util.Map;
 
 public class YMLReader {
-    public String getXPath(String str, String filename){
+    //returns the xpath of required element given by name "name" and will search in "filename".yml
+    //object_repository
+    public String getXPath(String name, String filename){
         Yaml yaml = new Yaml();
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
-        Map<String, String > map = yaml.load(inputStream);
-        String xpath = map.get(str);
+        Map<String, String > map = yaml.load(inputStream);      //map for storing the key-value pair of yaml file
+        String xpath = map.get(name);
         return xpath;
     }
 
+    //returns value of key in map of map in yml
     public String getInfo(String key1,String key2, String filename){
         Yaml yaml = new Yaml();
         InputStream inputStream = this.getClass()
