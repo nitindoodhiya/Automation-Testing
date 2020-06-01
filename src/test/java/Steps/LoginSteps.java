9236c9810afa6a43404bd366678382aba13950e1
Nick Nitin <nitindoodhiya@gmail.com>
2020-06-01 10:49:15 +0530

    Assert of Junit is imported

--- src/test/java/Steps/LoginSteps.java
+++ src/test/java/Steps/LoginSteps.java
@@ -6,7 +6,8 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.datatable.DataTable;
-import org.testng.Assert;
+import org.junit.Assert;
+
 
 import java.util.List;
 
