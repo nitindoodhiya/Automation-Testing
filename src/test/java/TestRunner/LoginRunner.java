65ad1fd8ad3926c1390df52d8740998e39c7efda
Nick Nitin <nitindoodhiya@gmail.com>
2020-06-11 22:28:16 +0530

    Update LoginRunner.java

--- src/test/java/TestRunner/LoginRunner.java
+++ src/test/java/TestRunner/LoginRunner.java
@@ -1,15 +1,12 @@
-package TestRunner;
+package testRunner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/Features/login.feature", glue = "Steps")
+@CucumberOptions(features = "src/test/java/features", glue = "steps")
 
 public class LoginRunner {
-	public static void main(String[] args)
-	{
-		System.out.print("Hello");
-	}
+
 }
