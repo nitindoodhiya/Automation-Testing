7c12dfff7c3fb019cc23be1182401bfc4873d7fa
Nick Nitin <nitindoodhiya@gmail.com>
2020-06-11 22:25:04 +0530

    Update LoginSteps.java

--- src/test/java/Steps/LoginSteps.java
+++ src/test/java/Steps/LoginSteps.java
@@ -1,22 +1,18 @@
-package Steps;
+package steps;
 
-import Base.BaseUtil;
-import io.appium.java_client.touch.offset.PointOption;
+import base.BaseUtil;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.datatable.DataTable;
-import org.apache.log4j.PropertyConfigurator;
-import io.appium.java_client.TouchAction;
 import org.junit.Assert;
 
-import java.io.IOException;
 import java.util.List;
 
-public class LoginSteps  {
+public class LoginSteps {
 
-    private BaseUtil base;
+    private final BaseUtil base;
 
     public LoginSteps(BaseUtil base) {
 
@@ -25,20 +21,22 @@ public class LoginSteps  {
 
     @Given("App is open")
     public void appIsOpen() {
-        base.logger.info("App Should have opened");
+
+        BaseUtil.logger.info("App Should have opened");
     }
 
     @When("User Clicks on {string}")
     public void userCLicksOnString(String touch) {
-        base.logger.info("User to click on: "+ touch);
+        BaseUtil.logger.info("User to click on: " + touch);
         base.tap(touch);
     }
 
     @Then("User Navigated to Homescreen")
     public void userNavigatedToHomescreen() {
 
-        base.logger.info("User navigated to HomeScreen ");
-        base.tap("SEARCH_INPUT_FIELD_BUTTON");
+        BaseUtil.logger.info("User navigated to HomeScreen ");
+        System.out.println(base.androidDriver.currentActivity());
+        Assert.assertTrue(base.tap("SEARCH_INPUT_FIELD_BUTTON"));
     }
 
     @And("User enters following fields")
@@ -51,35 +49,44 @@ public class LoginSteps  {
     @And("User enters in {string} from {string}")
     public void userEntersInFrom(String field, String input) {
 
-        base.sendkeys(field,base.reader.getInfo(input,field,base.dataSet));
+        base.sendkeys(field, BaseUtil.reader.getInfo(input, field, BaseUtil.dataSet));
     }
 
 
-    @And("User search for another product")
+    @And("User search for these products and add them to cart")
     public void userSearchForAnotherProduct(DataTable dataTable) throws InterruptedException {
-        List<List<String>>data=dataTable.asLists(String.class);
-        for(int ind=0;ind<data.size();ind++)
-        {
-            base.tap(data.get(ind).get(0)); //user clicked on SEARCH_BAR
-            base.tap(data.get(ind).get(1)); //user clicked on SEARCH_TAP
-            base.sendkeys(data.get(ind).get(1),data.get(ind).get(2)); //user searched for product
-            Thread.sleep(5000);
 
-            PointOption pointStart = PointOption.point(984, 1992); //User pressed enter key
-            (new TouchAction(base.androidDriver)).tap(pointStart).perform();
-            Thread.sleep(5000);
+        System.out.println(base.androidDriver.currentActivity());
+        List<List<String>> data = dataTable.asLists(String.class);
+        for (int ind = 0; ind < data.size(); ind++) {
 
-            base.tap(data.get(ind).get(3)); // user taps on first element
-            Thread.sleep(5000);
-            base.scrolldowntofind(data.get(ind).get(4));//user adds to cart
-            Thread.sleep(5000);
+            base.tap(data.get(ind).get(1));                                 //user clicked on SEARCH_TAP
+            base.sendkeys(data.get(ind).get(1), data.get(ind).get(2));       //user searched for product
+            Thread.sleep(2000);                                        //waiting for keyboard to visible
+            base.userPressEnterKeyforaplphakeys();                                       //user press enter key
+
+            //selecting only prime products
+            base.tap("PRIME_PRODUCT");
+            Thread.sleep(3000);
+
+            base.tap(data.get(ind).get(3));
+
+                // user taps on first element
+            Thread.sleep(4000);                                         //waiting for product page to visible
+            base.scrolldowntofind(data.get(ind).get(4));                    //user adds to cart
+            Thread.sleep(10000);
+            if (ind != data.size() - 1)
+                base.tap(data.get(ind).get(0));                             //user clicked on SEARCH_BAR
+            else
+                BaseUtil.logger.info("Done adding to cart");
         }
 
     }
 
     @And("User enters {string} in {string}")
     public void userEntersIn(String item, String box) throws InterruptedException {
-        base.sendkeys(box,item);
+        base.sendkeys(box, item);
+        base.userPressEnterKeyfornumerickeys();
         Thread.sleep(500);
 
     }
